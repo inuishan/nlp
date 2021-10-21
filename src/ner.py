7df0dac23d612fd9dd3acf2e16973b51b06648a6
ijain-ontic <ijain@ontic.ai>
2021-10-21 11:16:31 +0530

    initial commit

---
+++ src/ner.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+
+def extract_token(text):
+    ner_results = nlp(text)
+    print(text)
