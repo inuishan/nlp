c79d27756397b78fc6c938c5062ddd4c9b4c3848
Abhishek Gupta <abhishek.gupta@ontic.ai>
2021-10-21 18:38:24 +0530

    adds classify

---
+++ src/classify.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("valhalla/distilbart-mnli-12-3")
+model = AutoModelForSequenceClassification.from_pretrained("valhalla/distilbart-mnli-12-3")
+
+classifier = pipeline("zero-shot-classification",
+                      model="valhalla/distilbart-mnli-12-3")
+candidate_labels = ['High Inflow of Letters', 'Person seen keeping activity watch', 'Person seen in non-business hours',
+                    'Unattended Vehicle in Parking', 'Package Without Name']
+
+
+def classify(text):
+    res = classifier(text, candidate_labels)
+    return res['labels'][0]
+
+
+if __name__ == "__main__":
+    while True:
+        classify('Man who was armed with a knife at an hospital in Dallas, Texas, has been shot dead by police; no officers injured; special unit investigating the case')
